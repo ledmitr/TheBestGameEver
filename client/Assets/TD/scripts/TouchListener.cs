ddc4fcb4a4097038283384f2fb5776759434fa6b
perminov-aleksandr <perminov.a@mail.ru>
2015-10-02 03:40:38 +0300

    Client application project was converted to Unity 5.
    Added simple Game scene, touch event listener on it.

---
+++ client/Assets/TD/scripts/TouchListener.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class TouchListener : MonoBehaviour
+{
+    private string ChooseCircleTagName = "chooseCircle";
+    private const string TowerTagName = "Tower";
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
+        {
+            //declare a variable of RaycastHit struct
+            RaycastHit hit;
+            //Create a Ray on the tapped / clicked position
+            Ray ray;
+            //for unity editor
+#if UNITY_EDITOR
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //for touch device
+#elif (UNITY_ANDROID || UNITY_IPHONE || UNITY_WP8)
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+#endif
+            //Check if the ray hits any collider
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(ProcessHit(hit));
+            }
+        }
+    }
+
+    private IEnumerator ProcessHit(RaycastHit hit)
+    {
+        if (hit.collider.tag == TowerTagName)
+        {
+            var circles = GameObject.FindGameObjectsWithTag(ChooseCircleTagName);
+            foreach (var circleRendererL in circles.Select(x => x.GetComponent<Renderer>()))
+            {
+                circleRendererL.enabled = false;
+                yield return 0;
+            }
+
+            var circleRenderer = hit.transform.gameObject.GetComponentsInChildren<Renderer>().FirstOrDefault(x => x.gameObject.tag == ChooseCircleTagName);
+            if (circleRenderer != null)
+                circleRenderer.enabled = !circleRenderer.enabled; 
+        }
+    }
+}
+
+
+
