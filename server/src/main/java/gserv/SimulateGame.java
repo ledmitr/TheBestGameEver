47bf06c629a3de4d225be9cdc6099b7ced3e23d6
Igor Brodt <programist.igor@rambler.ru>
2015-12-23 07:29:00 +0300

    Деплой

--- server/src/main/java/gserv/SimulateGame.java
+++ server/src/main/java/gserv/SimulateGame.java
@@ -24,12 +24,12 @@ public class SimulateGame extends Thread
     /**
      * Ширина карты
      */
-    public static final int MAP_WIDTH = 9;
+    public static final int MAP_WIDTH = 100;
 
     /**
      * Высота карты
      */
-    public static final int MAP_HEIGHT = 9;
+    public static final int MAP_HEIGHT = 100;
 
     /**
      * Коллекция обектов атакующего игрока
